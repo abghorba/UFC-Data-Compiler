b4b5b49ad87d66856477e90df1912673c5f894ae
Andrew Ghorbani <andrewbghorbani@gmail.com>
2023-04-26 20:15:30 -0700

    add pre-commit hooks to use black and isort formatting

--- initialize.sh
+++ initialize.sh
@@ -4,7 +4,11 @@ if [ ! -d "env" ]; then
     echo "Creating virtual environment: env"
     python3 -m venv env
     source env/bin/activate
+    echo "Upgrading pip..."
+    python3 -m pip install --upgrade pip
     echo "Installing dependencies..."
     pip install -r requirements.txt
+    echo "Installing pre-commit hooks..."
+    pre-commit install
     echo "Done!"
 fi
